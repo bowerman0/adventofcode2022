d37026fe559587476689821f4c05e1f12c16698a
Michael Bowerman <michael.bowerman@gmail.com>
2022-12-10 20:24:22 -0800

    day 2, part 1

---
+++ day2/src/DayTwo.java
@@ -0,0 +1,77 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class DayTwo {
+    public static void main(String[] args) {
+        System.out.println("score: " + read());
+    }
+    private static int read() {
+        try(BufferedReader reader =
+                    new BufferedReader(new FileReader("input.txt"))) {
+            int score = 0;
+            for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+                switch (line.charAt(0)) {
+                    case 'A':
+                        switch (line.charAt(2)) {
+                            case 'X':
+                                score += 1;
+                                score += 3;
+                                break;
+                            case 'Y':
+                                score += 2;
+                                score += 6;
+                                break;
+                            case 'Z':
+                                score += 3;
+                                break;
+                            default:
+                                break;
+                        }
+                        break;
+                    case 'B':
+                        switch (line.charAt(2)) {
+                            case 'X':
+                                score += 1;
+                                break;
+                            case 'Y':
+                                score += 2;
+                                score += 3;
+                                break;
+                            case 'Z':
+                                score += 3;
+                                score += 6;
+                                break;
+                            default:
+                                break;
+                        }
+                        break;
+                    case 'C':
+                        switch (line.charAt(2)) {
+                            case 'X':
+                                score += 1;
+                                score += 6;
+                                break;
+                            case 'Y':
+                                score += 2;
+                                break;
+                            case 'Z':
+                                score += 3;
+                                score += 3;
+                                break;
+                            default:
+                                break;
+                        }
+                        break;
+                    default:
+                        throw new IllegalStateException("Unexpected value: " + line.charAt(0));
+                }
+            }
+            return score;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
+}
