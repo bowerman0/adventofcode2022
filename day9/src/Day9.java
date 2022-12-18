285449cf9fb3f840a4520e7946b9261fbb1e7062
Michael Bowerman <michael.bowerman@gmail.com>
2022-12-18 13:39:13 -0800

    Day 9, part 2

--- day9/src/Day9.java
+++ day9/src/Day9.java
@@ -1,17 +1,16 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Day9 {
+    private static final int TOTAL_KNOTS = 10;
     static class Rope {
-        List<Integer> head;
-        List<Integer> tail;
+        List<List<Integer>> knots;
     }
     public static void main(String[] args) {
-        System.out.println("total: " + read());
+        System.out.println("total    : " + read());
+        System.out.println("target(2): 5710");
     }
 
     private static int read() {
@@ -19,45 +18,28 @@ public class Day9 {
                      new BufferedReader(new FileReader("input.txt"))) {
             Set<List<Integer>> tailPositions = new HashSet<>();
             Rope rope = new Rope();
-            rope.head = List.of(0,0);
-            rope.tail = List.of(0,0);
-            tailPositions.add(rope.tail);
+            rope.knots = new ArrayList<>(Collections.nCopies(TOTAL_KNOTS, List.of(0,0)));
+            tailPositions.add(rope.knots.get(TOTAL_KNOTS-1));
             for (String line = reader.readLine(); line != null; line = reader.readLine()) {
                 switch (line.charAt(0)) {
                     case 'L':
                         for (int i=0; i<Integer.parseInt(line.substring(2)); ++i) {
                             moveLeft(tailPositions, rope);
-                            if (Math.abs(rope.head.get(0) - rope.tail.get(0)) > 1 ||
-                                    Math.abs(rope.head.get(1) - rope.tail.get(1)) > 1) {
-                                System.out.println(rope.head + ", " + rope.tail);
-                            }
                         }
                         break;
                     case 'R':
                         for (int i=0; i<Integer.parseInt(line.substring(2)); ++i) {
                             moveRight(tailPositions, rope);
-                            if (Math.abs(rope.head.get(0) - rope.tail.get(0)) > 1 ||
-                                    Math.abs(rope.head.get(1) - rope.tail.get(1)) > 1) {
-                                System.out.println(rope.head + ", " + rope.tail);
-                            }
                         }
                         break;
                     case 'U':
                         for (int i=0; i<Integer.parseInt(line.substring(2)); ++i) {
                             moveUp(tailPositions, rope);
-                            if (Math.abs(rope.head.get(0) - rope.tail.get(0)) > 1 ||
-                                    Math.abs(rope.head.get(1) - rope.tail.get(1)) > 1) {
-                                System.out.println(rope.head + ", " + rope.tail);
-                            }
                         }
                         break;
                     case 'D':
                         for (int i=0; i<Integer.parseInt(line.substring(2)); ++i) {
                             moveDown(tailPositions, rope);
-                            if (Math.abs(rope.head.get(0) - rope.tail.get(0)) > 1 ||
-                                    Math.abs(rope.head.get(1) - rope.tail.get(1)) > 1) {
-                                System.out.println(rope.head + ", " + rope.tail);
-                            }
                         }
                         break;
                     default:
@@ -74,73 +56,80 @@ public class Day9 {
     }
 
     private static void moveLeft(Set<List<Integer>> tailPositions, Rope rope) {
-        rope.head = List.of(rope.head.get(0)-1, rope.head.get(1));
-        if (rope.tail.get(0) - rope.head.get(0) > 1) {
-            switch (rope.head.get(1) - rope.tail.get(1)) {
-                case -1: // move down
-                    rope.tail = List.of(rope.tail.get(0)-1, rope.tail.get(1)-1);
-                    break;
-                case 1: // move up
-                    rope.tail = List.of(rope.tail.get(0)-1, rope.tail.get(1)+1);
-                    break;
-                default:
-                    rope.tail = List.of(rope.tail.get(0)-1, rope.tail.get(1));
-                    break;
-            }
-            tailPositions.add(rope.tail);
+        rope.knots.set(0, List.of(rope.knots.get(0).get(0)-1, rope.knots.get(0).get(1)));
+        for (int i=1; i < rope.knots.size() && gap(rope, i); ++i) {
+            int xIncr = xIncrement(rope, i);
+            int yIncr = yIncrement(rope, i);
+            rope.knots.set(i, List.of(rope.knots.get(i).get(0)+xIncr, rope.knots.get(i).get(1)+yIncr));
         }
+        tailPositions.add(rope.knots.get(TOTAL_KNOTS-1));
     }
+
     private static void moveRight(Set<List<Integer>> tailPositions, Rope rope) {
-        rope.head = List.of(rope.head.get(0)+1, rope.head.get(1));
-        if (rope.head.get(0) - rope.tail.get(0) > 1) {
-            switch (rope.head.get(1) - rope.tail.get(1)) {
-                case -1: // move down
-                    rope.tail = List.of(rope.tail.get(0)+1, rope.tail.get(1)-1);
-                    break;
-                case 1: // move up
-                    rope.tail = List.of(rope.tail.get(0)+1, rope.tail.get(1)+1);
-                    break;
-                default:
-                    rope.tail = List.of(rope.tail.get(0)+1, rope.tail.get(1));
-                    break;
-            }
-            tailPositions.add(rope.tail);
+        rope.knots.set(0, List.of(rope.knots.get(0).get(0)+1, rope.knots.get(0).get(1)));
+        for (int i=1; i < rope.knots.size() && gap(rope, i); ++i) {
+            int xIncr = xIncrement(rope, i);
+            int yIncr = yIncrement(rope, i);
+            rope.knots.set(i, List.of(rope.knots.get(i).get(0)+xIncr, rope.knots.get(i).get(1)+yIncr));
         }
+        tailPositions.add(rope.knots.get(TOTAL_KNOTS-1));
     }
 
     private static void moveUp(Set<List<Integer>> tailPositions, Rope rope) {
-        rope.head = List.of(rope.head.get(0), rope.head.get(1)+1);
-        if (rope.head.get(1) - rope.tail.get(1) > 1) {
-            switch (rope.head.get(0) - rope.tail.get(0)) {
-                case -1: // move left
-                    rope.tail = List.of(rope.tail.get(0)-1, rope.tail.get(1)+1);
-                    break;
-                case 1: // move right
-                    rope.tail = List.of(rope.tail.get(0)+1, rope.tail.get(1)+1);
-                    break;
-                default:
-                    rope.tail = List.of(rope.tail.get(0), rope.tail.get(1)+1);
-                    break;
-            }
-            tailPositions.add(rope.tail);
+        rope.knots.set(0, List.of(rope.knots.get(0).get(0), rope.knots.get(0).get(1)+1));
+        for (int i=1; i < rope.knots.size() && gap(rope, i); ++i) {
+            int xIncr = xIncrement(rope, i);
+            int yIncr = yIncrement(rope, i);
+            rope.knots.set(i, List.of(rope.knots.get(i).get(0)+xIncr, rope.knots.get(i).get(1)+yIncr));
         }
+        tailPositions.add(rope.knots.get(TOTAL_KNOTS-1));
     }
 
     private static void moveDown(Set<List<Integer>> tailPositions, Rope rope) {
-        rope.head = List.of(rope.head.get(0), rope.head.get(1)-1);
-        if (rope.tail.get(1) - rope.head.get(1) > 1) {
-            switch (rope.head.get(0) - rope.tail.get(0)) {
-                case -1: // move left
-                    rope.tail = List.of(rope.tail.get(0)-1, rope.tail.get(1)-1);
-                    break;
-                case 1: // move right
-                    rope.tail = List.of(rope.tail.get(0)+1, rope.tail.get(1)-1);
-                    break;
-                default:
-                    rope.tail = List.of(rope.tail.get(0), rope.tail.get(1)-1);
-                    break;
-            }
-            tailPositions.add(rope.tail);
+        rope.knots.set(0, List.of(rope.knots.get(0).get(0), rope.knots.get(0).get(1)-1));
+        for (int i=1; i < rope.knots.size() && gap(rope, i); ++i) {
+            int xIncr = xIncrement(rope, i);
+            int yIncr = yIncrement(rope, i);
+            rope.knots.set(i, List.of(rope.knots.get(i).get(0)+xIncr, rope.knots.get(i).get(1)+yIncr));
         }
+        tailPositions.add(rope.knots.get(TOTAL_KNOTS-1));
+    }
+
+    private static int xIncrement(Rope rope, int tail) {
+        int dx = rope.knots.get(tail-1).get(0) - rope.knots.get(tail).get(0);
+        if (dx <= -1) {
+            return -1;
+        } else if (dx >= 1) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    private static int yIncrement(Rope rope, int tail) {
+        int dy = rope.knots.get(tail-1).get(1) - rope.knots.get(tail).get(1);
+        if (dy <= -1) {
+            return -1;
+        } else if (dy >= 1) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    private static boolean gap(Rope rope, int tail) {
+        return gapAbove(rope, tail) || gapBelow(rope, tail) || gapLeft(rope, tail) || gapRight(rope, tail);
+    }
+    private static boolean gapRight(Rope rope, int tail) {
+        return rope.knots.get(tail-1).get(0) - rope.knots.get(tail).get(0) > 1;
+    }
+    private static boolean gapLeft(Rope rope, int tail) {
+        return rope.knots.get(tail).get(0) - rope.knots.get(tail-1).get(0) > 1;
+    }
+    private static boolean gapAbove(Rope rope, int tail) {
+        return rope.knots.get(tail-1).get(1) - rope.knots.get(tail).get(1) > 1;
+    }
+    private static boolean gapBelow(Rope rope, int tail) {
+        return rope.knots.get(tail).get(1) - rope.knots.get(tail-1).get(1) > 1;
     }
 }
